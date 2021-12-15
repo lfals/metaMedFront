dc2d0e30733796a60b4953b315a5d45bd466e01a
Luis Amorim <luisfelipeamorim@hotmail.com>
2021-12-14 22:04:09 -0300

    feat: Global Style

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import GlobalStyle from './globalStyles';
 import {Dashboard} from "./view"
 
 ReactDOM.render(
   <React.StrictMode>
+     <GlobalStyle />
    <BrowserRouter> 
     <Routes>
       <Route path="/" exact element={<Dashboard />} />
